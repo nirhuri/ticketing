f2a6fa52b370e9a53b620f104eaf0640bd460d7d
Nir Huri <hurinir@gmail.com>
2022-03-08 02:09:21 +0200

    Setting up header and singout

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,16 +1,30 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default ({ currentUser }) => {
-    return (
-        <nav className="navbar navbar-light bg-light">
-            <Link href='/'>
-                <a className='navbar-brand'>GitTix</a>
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser  && { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter(linkConfig => linkConfig)
+    .map(({ label, href }) => {
+        return (
+          <li key={href} className="nav-item">
+            <Link href={href}>
+              <a className="nav-link">{label}</a>
             </Link>
-            <div className='d-flex justify-content-end'>
-                <ul className='nav d-flex align-items-center'>
-                    {currentUser ? 'Sign out' : 'Signin in/up'}
-                </ul>
-            </div>
-        </nav>
-    )
-}
+          </li>
+        );
+    });
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <Link href="/">
+        <a className="navbar-brand">GitTix</a>
+      </Link>
+      <div className="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">{links}</ul>
+      </div>
+    </nav>
+  );
+};
