37b2e4efd440ea6a1ecaef6cbcd66fee8fcf6ac2
Nir Huri <hurinir@gmail.com>
2022-03-10 22:10:52 +0200

    Adding tests

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,22 @@
+import requrest from 'supertest';
+import { app } from '../../app';
+
+it('has a route handler listening to /api/tickets for post request', async () => {
+
+});
+
+it('can only be accessed if the user is signed in', () => {
+
+});
+
+it('returns an error if an invalid title is provided', () => {
+
+});
+
+it('returns an error if an invalid price is provided', async () => {
+
+});
+
+it('its read a ticket with valid input', async () => {
+
+});
