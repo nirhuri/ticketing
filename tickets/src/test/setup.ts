b89804cbc4cd24efcc294b5542ecfe5c4fbda248
Nir Huri <hurinir@gmail.com>
2022-03-12 23:46:54 +0200

    testing update tickets api

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -34,7 +34,7 @@ afterAll(async () => {
 
 global.signin = () => {
   const payload = {
-    id: 'jhfrtxyrcoui',
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com'
   }
 
