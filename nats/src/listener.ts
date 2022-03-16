1e1aaf7be85591a68851de6527e27c06258c89ba
Nir Huri <hurinir@gmail.com>
2022-03-17 01:01:30 +0200

    Random nats id for multiple connections

--- nats/src/listener.ts
+++ nats/src/listener.ts
@@ -1,8 +1,9 @@
-import nats from "node-nats-streaming";
+import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from 'crypto';
 
 console.clear();
 
-const stan = nats.connect("ticketing", "123", {
+const stan = nats.connect("ticketing", randomBytes(4).toString('hex'), {
   url: "http://localhost:4222",
 });
 
@@ -11,7 +12,13 @@ stan.on("connect", () => {
     
     const subscription = stan.subscribe('ticket:created');
 
-    subscription.on('message', (msg) => {
-        console.log('message recieved')
+    subscription.on('message', (msg: Message) => {
+        const data = msg.getData();
+
+        if (typeof data === 'string') {
+            console.log(
+                `Received event #${msg.getSequence()}, with data: ${data}`
+            );
+        }
     });
 });
