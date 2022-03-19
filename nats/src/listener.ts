cafe2b9787d5ba0cb7ac949b686b9dcaa0136723
Nir Huri <hurinir@gmail.com>
2022-03-19 20:24:48 +0200

    nats configs

--- nats/src/listener.ts
+++ nats/src/listener.ts
@@ -1,24 +1,39 @@
 import nats, { Message } from "node-nats-streaming";
-import { randomBytes } from 'crypto';
+import { randomBytes } from "crypto";
 
 console.clear();
 
-const stan = nats.connect("ticketing", randomBytes(4).toString('hex'), {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
     console.log("Listener connected to nats");
     
-    const subscription = stan.subscribe('ticket:created');
 
-    subscription.on('message', (msg: Message) => {
-        const data = msg.getData();
+    stan.on('close', () => {
+        console.log('NATS connection closed!');
+        process.exit();
+    })
 
-        if (typeof data === 'string') {
-            console.log(
-                `Received event #${msg.getSequence()}, with data: ${data}`
-            );
-        }
-    });
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+    options
+  );
+
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === "string") {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+
+    msg.ack();
+  });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
