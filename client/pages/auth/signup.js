80e74395b6527b438b4cfcaa2d1b3b0329d93f8d
Nir Huri <hurinir@gmail.com>
2022-04-07 13:44:22 +0300

    new orders and tickets pages

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Router from "next/router";
-
 import useRequest from "../../hooks/use-request";
 
 export default () => {
@@ -12,18 +11,19 @@ export default () => {
     body: {
       email,
       password,
-      },
-    onSuccess: () => Router.push("/")
+    },
+    onSuccess: () => Router.push("/"),
   });
 
   const onSubmit = async (event) => {
     event.preventDefault();
+
     await doRequest();
   };
 
   return (
     <form onSubmit={onSubmit}>
-      <h1>Sign up</h1>
+      <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
         <input
